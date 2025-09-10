abcf0c34d0bf6b0069c08782eeb6134da45f8012
moriaki1983 <124275584+moriaki1983s-product@users.noreply.github.com>
2025-09-10 15:15:47 +0900

    Add files via upload

---
+++ modules/engines/core/tagnet_builder.py
@@ -0,0 +1,60 @@
+# coding: utf-8
+
+
+
+
+# 既成のモジュールをインポートする.
+import os
+import sys
+
+# 独自のモジュールをインポートする.
+# import modules.intelligences.reg_unit as reg
+# import modules.intelligences.cog_unit as cog
+# import modules.intelligences.emo_unit as emo
+# import modules.intelligences.sim_unit as sim
+# import modules.intelligences.etc_unit as etc
+
+
+
+
+# Sunieエンジンのクラスを宣言・定義する.
+class TagnetBuilder:
+    _instance = None
+    dat = None
+
+    # インスタンス生成のためのメソッドを宣言・定義する.
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(TagnetBuilder, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+
+    # インスタンス初期化のためのメソッドを宣言・定義する.
+    def __init__(self):
+        # self.reg = reg.RegUnit()
+        # self.cog = cog.CogUnit()
+        # self.emo = emo.EmoUnit()
+        # self.sim = sim.SimUnit()
+        # self.etc = etc.EtcUnit()
+        self.dat = None
+
+    # データを設定するためのメソッドを宣言・定義する.
+    def set_data(self, dat):
+        self.dat = dat
+
+    # データを取得するためのメソッドを宣言・定義する.
+    def get_data(self):
+        return self.dat
+
+    # データを消去するためのメソッドを宣言・定義する.
+    def clear_data(self):
+        self.dat = None
+
+    # データを解析するためのメソッドを宣言・定義する.
+    def analytical_drive(self):
+        anlyzd_dat = self.dat
+        return anlyzd_dat
+
+    # データを生成するためのメソッドを宣言・定義する.
+    def generative_drive(self):
+        gnrtd_dat = self.dat
+        return gnrtd_dat
