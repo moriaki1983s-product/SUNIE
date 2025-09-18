78e1329212f05a0136f332d9445eda60488308f4
moriaki1983 <124275584+moriaki1983s-product@users.noreply.github.com>
2025-09-18 13:22:52 +0900

    Add files via upload

--- modules/utilities/random.py
+++ modules/utilities/random.py
@@ -5,16 +5,18 @@
 
 # 既成のモジュールをインポートする.
 import random
-import numpy as np
+# import numpy as np
 
 
 
-#ランダムに決定される状態値を[str]型で取得する.
+
+# ランダムに決定される状態値を[str]型で取得する.
 def random_select(stts):
-    random.seed(None) # シードをリセット
+    random.seed(None)
     return random.choice(stts)
 
 
-#ランダムに決定される複数の状態値を[str-list]型で取得する.
+# ランダムに決定される複数の状態値を[str-list]型で取得する.
 def random_selects(stts, wghts, smpl_num):
+    random.seed(None)
     return random.choices(stts, weights=wghts, k=smpl_num)
